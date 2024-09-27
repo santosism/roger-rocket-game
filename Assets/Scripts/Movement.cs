9588474ae24edabbbfde7fb3ea5044cea5e5bf35
Ismael Santos <isant028@fiu.edu>
2024-09-27 03:11:13 -0400

    Added thrusters and main engine particle effects to Rocket prefab, and added code logic to turn on and off effects in game

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,6 +12,9 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 100f;
     [SerializeField] float rotationThrust = 1f;
     [SerializeField] AudioClip mainEngine; 
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem leftThrusterParticles;
+    [SerializeField] ParticleSystem rightThrusterParticles; 
     Rigidbody rb; 
 
     AudioSource audioSource;
@@ -40,24 +43,44 @@ public class Movement : MonoBehaviour
             {
                 audioSource.PlayOneShot(mainEngine);
             }
+
+            if(!mainEngineParticles.isPlaying)
+            {
+                mainEngineParticles.Play();
+            }
             
         }
         else
             {
                 audioSource.Stop();
+                mainEngineParticles.Stop();
             }
     }
 
     void ProcessRotation()
     {
         if (Input.GetKey(KeyCode.A))
+        
         {
             ApplyRotation(rotationThrust);
+             if(!rightThrusterParticles.isPlaying)
+            {
+                rightThrusterParticles.Play();
+            }
 
         }
         else if (Input.GetKey(KeyCode.D))
         {
             ApplyRotation(-rotationThrust);
+            if(!leftThrusterParticles.isPlaying)
+            {
+                leftThrusterParticles.Play();
+            }
+        }
+        else
+        {
+            rightThrusterParticles.Stop();
+            leftThrusterParticles.Stop();
         }
     }
 
